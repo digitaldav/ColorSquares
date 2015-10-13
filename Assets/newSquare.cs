ba6865c19b1c415df6b4ec2005307fdaee3557b9
David <digitaldavg@hotmail.com>
2015-10-13 20:52:15 +0100

    Early version

---
+++ Assets/newSquare.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class newSquare : MonoBehaviour {
+
+	public Vector3 startPosition;
+
+	public Object redSquare, blueSquare, greenSquare, yellowSquare;
+
+	private float number;
+
+
+	void Start () {
+	
+		startPosition = new Vector3 (0.0f, 1.0f, -7.0f);
+
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		number = Random.Range (0, 4);
+		print (number);
+		if (gameObject.tag.Equals (other.tag) && !other.gameObject.layer.Equals(gameObject.layer)) {
+			//score++
+			if(number==0){
+				Instantiate(redSquare, startPosition,  Quaternion.identity);
+			}else if(number==1){
+				Instantiate(blueSquare, startPosition,  Quaternion.identity);
+			}else if(number==2){
+				Instantiate(greenSquare, startPosition,  Quaternion.identity);
+			}else{
+				Instantiate(yellowSquare, startPosition,  Quaternion.identity);
+			}
+			Destroy (this.gameObject);
+
+		} else if( !other.gameObject.layer.Equals(gameObject.layer) ) {
+			//score--
+			if(number==0){
+				Instantiate(redSquare, startPosition,  Quaternion.identity);
+			}else if(number==1){
+				Instantiate(blueSquare, startPosition,  Quaternion.identity);
+			}else if(number==2){
+				Instantiate(greenSquare, startPosition,  Quaternion.identity);
+			}else{
+				Instantiate(yellowSquare, startPosition,  Quaternion.identity);
+			}
+			Destroy (this.gameObject);
+		}
+	}
+
+	
+}
